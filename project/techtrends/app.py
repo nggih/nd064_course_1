ca3c3380157bb9ef1bfa4f65bb88f76916f6823c
nggih <linggih.saputro@sci.ui.ac.id>
2021-07-04 15:27:50 +0700

    feat: completing the flask tasks

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,4 +1,5 @@
 import sqlite3
+import logging
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
@@ -36,13 +37,17 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
-      return render_template('404.html'), 404
+        app.logger.info(f'Article id does not exist: {post_id}')
+        return render_template('404.html'), 404
     else:
-      return render_template('post.html', post=post)
+        title = post[2]
+        app.logger.info(f'Existing article is retrieved: {title}')
+        return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info('About Us page is retrieved.')
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -60,11 +65,52 @@ def create():
                          (title, content))
             connection.commit()
             connection.close()
-
+            app.logger.info(f'New article is created: {title}')
             return redirect(url_for('index'))
 
     return render_template('create.html')
 
+@app.route("/healthz")
+def status():
+    try:
+        response = app.response_class(
+            response=json.dumps({"result": "OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+        )
+        app.logger.info('Status request successful')
+    except Exception:
+        response = app.response_class(
+            response=json.dumps({"result": "ERROR - unhealthy"}),
+            status=500,
+            mimetype='application/json'
+        )
+        app.logger.info('Status request error')
+    return response 
+
+@app.route('/metrics')
+def metrics():
+    connection = get_db_connection()
+    # check total posts
+    posts = connection.execute('SELECT * FROM posts').fetchall()
+    # count the query or any connections using trace callback
+    db_connection_counts = len([connection.set_trace_callback(print)]) 
+    response = app.response_class(
+        response=json.dumps({
+            "status": "success",
+            "data": {
+                "db_connection_count": db_connection_counts,
+                "post_count": len(posts),
+            }
+        }),
+        status=200,
+        mimetype='application/json'
+    )
+    connection.close()
+    app.logger.info('Metrics request successful')
+    return response
+
 # start the application on port 3111
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port='3111')
+    logging.basicConfig(filename='app.log', level=logging.DEBUG)
+    app.run(host='0.0.0.0', port='3111')
